9dcda695de0b69e2ae50b665e1dccc4e7db33230
ZamKaf <chadov@phystech.edu>
2018-11-19 22:18:01 +0300

    fixed

--- app/wsgi.py
+++ app/wsgi.py
@@ -6,8 +6,10 @@ import datetime
 def app(environ, start_response):
     time = str(datetime.datetime.now().time())
     info = json.dumps(
-        {"data": {"time": time, "url": environ["HTTP_HOST"].encode("utf-8")},
-         "content-type": "application/json"}
+        {"time": time, "url": environ["HTTP_HOST"] }
     )
     start_response('200 OK', [('content-type', 'application/json')])
-    return [info]
+    return [info.encode('utf-8')]
+
+
+#gunicorn --threads 20 --access-logfile - wsgi:app
